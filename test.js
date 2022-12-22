c96c6bd6830d7d31ccc5351d9c4fb18bd82d1bc8
Taiyaki-1b <107826374+Taiyaki-1b@users.noreply.github.com>
2022-12-22 23:18:03 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
 console.log('u')
+let request = new XMLHttpRequest();
+request.open('GET', 'https://www.example.com/data.txt', false);
+request.send(null);
+
+if (request.status == 200){
+  let data = request.responseText;
+  console.log(data);
+}
